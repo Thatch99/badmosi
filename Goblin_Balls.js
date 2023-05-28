aee8c416f7c0f26859ce950f5f9a0186c67eeeec
99% <sanjaysha216@gmail.com>
2023-05-28 13:01:48 +0530

    updated

--- Goblin_Balls.js
+++ Goblin_Balls.js
@@ -9,12 +9,26 @@
 //         console.error("Could not get IP address: ", error);
 //     });
 
-fetch('https://api.ipify.org?format=json')
-    .then(response => response.json())
-    .then(data => {
-        const ip_container = document.querySelector('#ip-container');
-        ip_container.textContent = data.ip;
-    })
-    .catch(error => {
-        console.error("Error retrieving IP address:", error);
-    });
+// fetch('https://api.ipify.org?format=json')
+//     .then(response => response.json())
+//     .then(data => {
+//         const ip_container = document.querySelector('#ip-container');
+//         ip_container.textContent = data.ip;
+//     })
+//     .catch(error => {
+//         console.error("Error retrieving IP address:", error);
+//     });
+
+
+const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+const apiUrl = 'https://api.ipify.org?format=json';
+
+fetch(proxyUrl + apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    const ip_container = document.querySelector('#ip-container');
+    ip_container.textContent = data.ip;
+  })
+  .catch(error => {
+    console.error("Error retrieving IP address:", error);
+  });
