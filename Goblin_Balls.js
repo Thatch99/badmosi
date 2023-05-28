203cf19282774bb37fa345ac6cb1a8ddacc87f23
99% <sanjaysha216@gmail.com>
2023-05-28 12:50:33 +0530

    updated

--- Goblin_Balls.js
+++ Goblin_Balls.js
@@ -1,10 +1,20 @@
-fetch("https://api.ipify.org?format=json")
+// fetch("https://api.ipify.org?format=json")
+//     .then(response => response.json())
+//     .then(data => {
+//         const ipAddress = data.ip;
+//         const ip_container = document.querySelector('#ip-container');
+//         ip_container.textContent = ipAddress;
+//     })
+//     .catch(error => {
+//         console.error("Could not get IP address: ", error);
+//     });
+
+fetch('https://api.ipify.org?format=json')
     .then(response => response.json())
     .then(data => {
-        const ipAddress = data.ip;
         const ip_container = document.querySelector('#ip-container');
-        ip_container.textContent = ipAddress; // Outputs the user's IP address to the console
+        ip_container.textContent = data.ip;
     })
     .catch(error => {
-        console.error("Could not get IP address: ", error);
+        console.error("Error retrieving IP address:", error);
     });
