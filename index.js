7e596eadde42bb7e4f22e14f7782464f00544f83
99% <sanjaysha216@gmail.com>
2023-05-30 23:03:37 +0530

    3rd button link attached

--- index.js
+++ index.js
@@ -19,13 +19,22 @@ function redirectToPage() {
 function redirectToGoblin() {
   window.location.href = 'Gobin_Balls.html';
 }
+function redirectToHail() {
+  window.location.href = 'Hail_NIgler.html';
+}
 
 const btn1_method = document.querySelector('.button-1');
 const btn2_method = document.querySelector('.button-2');
+const btn3_method = document.querySelector('.button-3');
 const flash_div = document.getElementById('flash');
 btn2_method.addEventListener('click',()=>{
   redirectToGoblin();
 })
+
+btn3_method.addEventListener('click',()=>{
+  redirectToHail();
+})
+
 btn1_method.addEventListener('click', () => {
   fullscreen();
   setTimeout(() => {
