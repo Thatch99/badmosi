38d6d7760227493516770becf037e406846f3768
99% <sanjaysha216@gmail.com>
2023-06-05 09:30:26 +0530

    updated

--- Hail_NIgler.js
+++ Hail_NIgler.js
@@ -1,7 +1,9 @@
+'use strict'
 const overlay_element = document.querySelector('.warning-overlay');
 
 const continue_btn = document.querySelector('.continue')
 
 continue_btn.addEventListener('click',function(){
+    document.documentElement.classList.add('overflow-scroll');
     overlay_element.classList.add('hidden')
 })
